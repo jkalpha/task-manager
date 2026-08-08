e178b29151610cb9423bb68624cead1e85f22917
10xdaemon <jdlservs@gmail.com>
2026-08-08 09:11:56 -0400

    choir: add doctring, add type hints

--- database.py
+++ database.py
@@ -1,49 +1,49 @@
-from typing import Any
-
-
 import sqlite3 as sqweel
 #from app import tasks
 
 DB_PATH = "task_manager.db"
 
 def connect_db():
+    """Helper function that opens the database connection."""
     return sqweel.connect("task_manager.db")
 
 def initialize_db():
-    with connect_db() as conn:
-        try:
-            cursor = conn.cursor()
-            # Create tasks table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS tasks(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title VARCHAR (50),
-                    completed BIT
-                )
-            """)
-            # Create users table
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS users(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR (60)    
-                )
-            """)
+    """Initializes schema with tasks and user tables."""
+    conn = connect_db()
+    # with connect_db() as conn:
+    try:
+        cursor = conn.cursor()
+        # Create tasks table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS tasks(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title VARCHAR (50),
+                completed BIT
+            )
+        """)
+        # Create users table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR (60)    
+            )
+        """)
 
-            # Importing data into tables to add persistence:
-            # for task in tasks:
-            #     cursor.execute(
-            #         "INSERT INTO tasks (id, title, completed) VALUES(?, ?, ?)",
-            #         (task["id"], task["title"], int(task["completed"]))
-            #     )
-            
-            # 
-            conn.commit()
+        # Importing data into tables to add persistence:
+        # for task in tasks:
+        #     cursor.execute(
+        #         "INSERT INTO tasks (id, title, completed) VALUES(?, ?, ?)",
+        #         (task["id"], task["title"], int(task["completed"]))
+        #     )
+        
+        # 
+        conn.commit()
 
-        except sqweel.Error as error:
-            print(f"An error occured: {error}")
+    except sqweel.Error as error:
+        print(f"An error occured: {error}")
 
-        finally:
-            conn.close()
+    finally:
+        conn.close()
 
 def get_tasks_all():
     with connect_db() as conn:
